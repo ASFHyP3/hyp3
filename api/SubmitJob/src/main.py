17dcac7fb614b475e64edde5201c7ccf2dfa5fc3
James Rine <jlrine2@alaska.edu>
2020-05-14 13:33:57 -0800

    flake8 compliance

--- api/SubmitJob/src/main.py
+++ api/SubmitJob/src/main.py
@@ -9,12 +9,12 @@ client: boto3 = boto3.client('batch')
 def lambda_handler(event, context):
     parameters = json.loads(event['body'])
     job = client.submit_job(jobName=parameters['granule'],
-                      jobQueue=getenv('JobQueue'),
-                      jobDefinition=getenv('JobDefinition'),
-                      parameters={
-                          'granule': parameters['granule']
-                      }
-                      )
+                            jobQueue=getenv('JobQueue'),
+                            jobDefinition=getenv('JobDefinition'),
+                            parameters={
+                                'granule': parameters['granule']
+                            }
+                            )
 
     response = {
         'Job Name': job['jobName'],
