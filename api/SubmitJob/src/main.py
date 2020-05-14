9a8802b2bd58ecf02309f27fe3abcbc9eca5f4a4
James Rine <jlrine2@alaska.edu>
2020-05-14 14:52:28 -0800

    readd job name

--- api/SubmitJob/src/main.py
+++ api/SubmitJob/src/main.py
@@ -9,12 +9,14 @@ BATCH_CLIENT = boto3.client('batch')
 def lambda_handler(event, context):
     parameters = json.loads(event['body'])
     job = BATCH_CLIENT.submit_job(
+        jobName=parameters['granule'],
         jobQueue=environ['JOB_QUEUE'],
         jobDefinition=environ['JOB_DEFINITION'],
         parameters=parameters
     )
     response = {
-        'jobId': job['jobId']
+        'jobId': job['jobId'],
+        'jobName': job['jobName']
     }
     return {
         'statusCode': 200,
