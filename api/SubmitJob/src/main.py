ed3776573217f735727fbf5fb125b6f5af5d3ba9
James Rine <jlrine2@alaska.edu>
2020-05-14 13:29:04 -0800

    use lambda to submit job

--- api/SubmitJob/src/main.py
+++ api/SubmitJob/src/main.py
@@ -1,5 +1,27 @@
+import json
+from os import getenv
+
 import boto3
 
-client = boto3.client('batch')
+client: boto3 = boto3.client('batch')
+
+
 def lambda_handler(event, context):
-    client.submit_job(event['body']['granule'], )
+    parameters = json.loads(event['body'])
+    job = client.submit_job(jobName=parameters['granule'],
+                      jobQueue=getenv('JobQueue'),
+                      jobDefinition=getenv('JobDefinition'),
+                      parameters={
+                          'granule': parameters['granule']
+                      }
+                      )
+
+    response = {
+        'Job Name': job['jobName'],
+        'Job Id': job['jobId']
+
+    }
+    return {
+        'statusCode': 200,
+        'body': json.dumps(response)
+    }
