f48f3dde0bee8d87fb8d3503f0e84f11afb0d478
James Rine <jlrine2@alaska.edu>
2020-05-19 15:50:33 -0800

    linting

--- api/hyp3_api/__init__.py
+++ api/hyp3_api/__init__.py
@@ -4,5 +4,5 @@ import connexion
 connexion_app = connexion.App(__name__)
 BATCH_CLIENT = boto3.client('batch')
 
-from hyp3_api import handlers, auth
+from hyp3_api import auth, handlers  # noqa Has to be at end of file or will cause circular import
 __all__ = ['handlers', 'connexion_app', 'BATCH_CLIENT', 'auth']
