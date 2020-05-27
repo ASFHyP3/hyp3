ef311465e992047fbf8e23447510a4358238dcfa
Andrew Johnston <asjohnston@alaska.edu>
2020-05-27 10:49:11 -0800

    add tests for lambda handler

---
+++ api/tests/test_lambda_handler.py
@@ -0,0 +1,30 @@
+import lambda_handler
+from flask_api import status
+
+
+def test_404_response():
+    event = {
+        'path': '/',
+        'httpMethod': 'GET',
+        'body': '',
+        'headers': {},
+        'requestContext': {},
+    }
+    response = lambda_handler.handler(event, None)
+    assert response['statusCode'] == status.HTTP_404_NOT_FOUND
+    assert response['headers']['Content-Type'] == 'application/problem+json'
+    assert response['isBase64Encoded'] == False
+
+
+def test_401_response():
+    event = {
+        'path': '/jobs',
+        'httpMethod': 'POST',
+        'body': '',
+        'headers': {},
+        'requestContext': {},
+    }
+    response = lambda_handler.handler(event, None)
+    assert response['statusCode'] == status.HTTP_401_UNAUTHORIZED
+    assert response['headers']['Content-Type'] == 'application/problem+json'
+    assert response['isBase64Encoded'] == False
