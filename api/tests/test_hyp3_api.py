54e56e87f22889918193fa6b5f883fb700687009
James Rine <jlrine2@alaska.edu>
2020-06-03 13:22:27 -0800

    remove job def and queue from test env

--- api/tests/test_hyp3_api.py
+++ api/tests/test_hyp3_api.py
@@ -45,8 +45,6 @@ def add_response(states_stub, granule, job_id='myJobId', email='john.doe@example
             'granule': granule,
         },
         'process_type': 'RTC_GAMMA',
-        'jobDefinition': 'arn:aws:batch:us-west-2:1234:job-definition/hyp3-develop-rtc-gamma:1',
-        'jobQueue': 'arn:aws:batch:us-west-2:1234:job-queue/hyp3-develop',
     }
     states_stub.add_response(
         method='start_execution',
