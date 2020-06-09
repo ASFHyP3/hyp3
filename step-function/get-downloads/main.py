300dda36fb0dd70f4010a9afad982a00a823b819
James Rine <jlrine2@alaska.edu>
2020-06-09 13:13:05 -0800

    add lamda function to do lookup and return list '

---
+++ step-function/get-downloads/main.py
@@ -0,0 +1,24 @@
+from os import environ
+from os.path import basename
+
+import boto3
+
+S3_CLIENT = boto3.client('s3')
+
+
+def get_download_url(key):
+    bucket = environ['BUCKET']
+    region = environ['AWS_REGION']
+    return f'https://{bucket}.s3.{region}.amazonaws.com/{key}'
+
+
+def lambda_handler(event, context):
+    objects = S3_CLIENT.list_objects_v2(Bucket=environ['BUCKET'], Prefix=event['job_id'])
+    return [
+        {
+            'url': get_download_url(item['Key']),
+            'size': item['Size'],
+            'filename': basename(item['key']),
+        }
+        for item in objects['Contents']
+    ]
