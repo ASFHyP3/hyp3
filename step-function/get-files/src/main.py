04bb75e0b5eca11fb9fd769587f5c4c6a3d56556
James Rine <jlrine2@alaska.edu>
2020-06-18 14:38:01 -0800

    remove handling for no key so that lambda will crash instead of inserting invalid records

--- step-function/get-files/src/main.py
+++ step-function/get-files/src/main.py
@@ -15,8 +15,6 @@ def get_download_url(key):
 
 def get_expiration_time(key):
     s3_object = S3_CLIENT.get_object(Bucket=environ['BUCKET'], Key=key)
-    if 'Expiration' not in s3_object:
-        return None
     expiration_string = s3_object['Expiration'].split('"')[1]
     expiration_datetime = datetime.strptime(expiration_string, '%a, %d %b %Y %H:%M:%S %Z')
     return int(expiration_datetime.timestamp())
