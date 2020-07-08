acaa0b5ecd71baac3bcf89864b85cd94e09e2156
James Rine <jlrine2@alaska.edu>
2020-07-08 10:10:19 -0800

    introduced util file in hyp3-api to seperate handlers from helper functions

--- api/tests/test_submit_job.py
+++ api/tests/test_submit_job.py
@@ -3,7 +3,7 @@ from os import environ
 
 from conftest import DEFAULT_USERNAME, login, make_db_record, make_job, setup_requests_mock, submit_batch
 from flask_api import status
-from hyp3_api.handlers import format_time
+from hyp3_api.util import format_time
 
 
 def test_submit_one_job(client, table):
