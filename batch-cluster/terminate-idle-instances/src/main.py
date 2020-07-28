afb515b3a61cf26e85bc06c2eed81deba85df877
Andrew Johnston <asjohnston@alaska.edu>
2020-07-27 17:26:37 -0800

    check for runnable jobs in terminate-idle-instances

--- batch-cluster/terminate-idle-instances/src/main.py
+++ batch-cluster/terminate-idle-instances/src/main.py
@@ -1,5 +1,3 @@
-from datetime import datetime, timedelta
-
 import boto3
 
 BATCH = boto3.client('batch')
@@ -7,23 +5,31 @@ ECS = boto3.client('ecs')
 EC2 = boto3.client('ec2')
 
 
-def lambda_handler(event, context):
-    compute_environment_arn = event['ComputeEnvironmentArn']
+def runnable_jobs_exist(job_queue_arn):
+    response = BATCH.list_jobs(jobQueue=job_queue_arn, jobStatus='RUNNABLE', maxResults=1)
+    return response['jobSummaryList'] != []
 
+
+def get_idle_instance_ids(compute_environment_arn):
     response = BATCH.describe_compute_environments(computeEnvironments=[compute_environment_arn])
     ecs_cluster_arn = response['computeEnvironments'][0]['ecsClusterArn']
 
-    registration_grace_timestamp = datetime.now() - timedelta(minutes=5)
-    response = ECS.list_container_instances(
-        cluster=ecs_cluster_arn,
-        filter=f'runningTasksCount==0 and registeredAt < {registration_grace_timestamp.strftime("%Y-%m-%dT%H:%M:%SZ")}'
-    )
+    response = ECS.list_container_instances(cluster=ecs_cluster_arn, filter='runningTasksCount==0')
     container_instance_arns = response['containerInstanceArns']
 
+    idle_instance_ids = []
     if container_instance_arns:
         response = ECS.describe_container_instances(cluster=ecs_cluster_arn, containerInstances=container_instance_arns)
-        ec2_instance_ids = [item['ec2InstanceId'] for item in response['containerInstances']]
-        print(ec2_instance_ids)
+        idle_instance_ids = [item['ec2InstanceId'] for item in response['containerInstances']]
+    return idle_instance_ids
 
-        response = EC2.terminate_instances(InstanceIds=ec2_instance_ids)
+
+def lambda_handler(event, context):
+    if runnable_jobs_exist(event['JobQueueArn']):
+        return
+
+    idle_instance_ids = get_idle_instance_ids(event['ComputeEnvironmentArn'])
+    if idle_instance_ids:
+        print(idle_instance_ids)
+        response = EC2.terminate_instances(InstanceIds=idle_instance_ids)
         print(response)
