765588cc9cf1936864783cb34a2088e6a7ec4b29
James Rine <jlrine2@alaska.edu>
2020-09-02 15:57:56 -0800

    api tests pass

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -45,13 +45,13 @@ def get_table_properties_from_template():
     return table_properties
 
 
-def make_job(granule='S1B_IW_SLC__1SDV_20200604T082207_20200604T082234_021881_029874_5E38',
+def make_job(granules=['S1B_IW_SLC__1SDV_20200604T082207_20200604T082234_021881_029874_5E38'],
              name='someName',
              job_type='RTC_GAMMA'):
     job = {
         'job_type': job_type,
         'job_parameters': {
-            'granule': granule,
+            'granules': granules,
             'resolution': 30.0,
         }
     }
@@ -73,7 +73,7 @@ def submit_batch(client, batch=None, validate_only=None):
 
 
 def make_db_record(job_id,
-                   granule='S1A_IW_SLC__1SDV_20200610T173646_20200610T173704_032958_03D14C_5F2B',
+                   granules=['S1A_IW_SLC__1SDV_20200610T173646_20200610T173704_032958_03D14C_5F2B'],
                    job_type='RTC_GAMMA',
                    user_id=DEFAULT_USERNAME,
                    request_time='2019-12-31T15:00:00+00:00',
@@ -88,7 +88,7 @@ def make_db_record(job_id,
         'user_id': user_id,
         'job_type': job_type,
         'job_parameters': {
-            'granule': granule,
+            'granules': granules,
         },
         'request_time': request_time,
         'status_code': status_code,
@@ -107,7 +107,8 @@ def make_db_record(job_id,
 
 
 def setup_requests_mock(batch):
-    granules = [job['job_parameters']['granule'] for job in batch]
+    granules = []
+    [[granules.append(granule) for granule in job['job_parameters']['granules']] for job in batch]
     cmr_response = {
         'feed': {
             'entry': [
