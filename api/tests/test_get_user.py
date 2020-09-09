f6de618c645f6f7b9fa95bbdd2cf73cf2948c7a3
James Rine <jlrine2@alaska.edu>
2020-09-09 09:23:24 -0800

    remove sorting from tests

--- api/tests/test_get_user.py
+++ api/tests/test_get_user.py
@@ -28,10 +28,10 @@ def test_get_user(client, table, monkeypatch):
             'limit': 25,
             'remaining': 21,
         },
-        'names': sorted([
+        'names': [
             'job1',
             'job2',
-        ]),
+        ],
     }
 
 
