4a0b8d169e658f7ede7fceb3872247833a35f626
James Rine <jlrine2@alaska.edu>
2020-09-18 11:29:28 -0800

    fix static analysis

--- apps/render_cf.py
+++ apps/render_cf.py
@@ -16,7 +16,7 @@ def render_template(template_file, job_types, env):
 
 def render_templates(job_types):
     env = get_env()
-    for template_file in Path('.').glob('*.j2'):
+    for template_file in Path('.').glob('**/*.j2'):
         render_template(template_file, job_types, env)
 
 
