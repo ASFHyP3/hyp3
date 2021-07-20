9a284f9469e03c4f8760066aef22aa81e7b62886
James <jlrine2@alaska.edu>
2021-07-20 14:28:41 -0800

    add dynamo package to flake8

--- lib/dynamo/setup.py
+++ lib/dynamo/setup.py
@@ -2,10 +2,13 @@ from setuptools import find_packages, setup
 
 setup(
     name='dynamo',
-
     license='BSD',
     include_package_data=True,
 
+    install_reqires=[
+        'boto3',
+        'dateutil',
+    ],
     python_requires='~=3.8',
 
     packages=find_packages(),
