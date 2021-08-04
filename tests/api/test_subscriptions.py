b2a2c7e75cdff17fe7148eb770d0139f617daee2
James <jlrine2@alaska.edu>
2021-08-04 14:42:02 -0800

    allow subscription POST requests without granules

--- tests/api/test_subscriptions.py
+++ tests/api/test_subscriptions.py
@@ -12,8 +12,6 @@ def test_post_subscription(client, tables):
         },
         'job_specification': {
             'job_parameters': {
-                'granules': ['S1A_IW_SLC__1SDV_20200720T172109_20200720T172128_033541_03E2FB_341F',
-                             'S1A_IW_SLC__1SDV_20200813T172110_20200813T172129_033891_03EE3F_2C3E', ],
                 'looks': '10x2',
                 'include_inc_map': True,
                 'include_look_vectors': True,
