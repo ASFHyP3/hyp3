7412bd89512ba9f5a6968620edb84bd03b381195
Andrew Johnston <asjohnston@alaska.edu>
2021-08-04 15:14:02 -0800

    filter by job type when searching for already-processed jobs

--- apps/process-new-granules/src/process_new_granules.py
+++ apps/process-new-granules/src/process_new_granules.py
@@ -14,7 +14,11 @@ def get_unprocessed_granules(subscription):
     search_results = asf_search.search(**subscription['search_parameters'])
     all_granules = [product.properties['sceneName'] for product in search_results]
 
-    processed_jobs = dynamo.jobs.query_jobs(subscription['user_id'], name=subscription['job_specification']['name'])
+    processed_jobs = dynamo.jobs.query_jobs(
+        subscription['user_id'],
+        job_type=subscription['job_specification']['job_type'],
+        name=subscription['job_specification']['name']
+    )
     processed_granules = itertools.chain([job['job_parameters']['granules'] for job in processed_jobs])
     return list(set(all_granules) - set(processed_granules))
 
