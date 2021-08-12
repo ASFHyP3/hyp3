2a9f8b49634f5de3338aff0c8f381cb104e5f5e1
James <jlrine2@alaska.edu>
2021-08-12 14:56:58 -0800

    initial stab at insar support

--- apps/process-new-granules/src/process_new_granules.py
+++ apps/process-new-granules/src/process_new_granules.py
@@ -1,6 +1,9 @@
+from copy import deepcopy
+from datetime import datetime, timedelta, timezone
 import itertools
 
 import asf_search
+import dateutil.parser
 
 import dynamo
 
@@ -18,22 +21,37 @@ def get_unprocessed_granules(subscription):
         name=subscription['job_specification']['name'],
         job_type=subscription['job_specification']['job_type'],
     )
-    processed_granules = itertools.chain(*[job['job_parameters']['granules'] for job in processed_jobs])
+    #  TODO This line could be made a lot clearer.
+    processed_granules = itertools.chain(*[job['job_parameters']['granules'][0] for job in processed_jobs])
     return list(set(all_granules) - set(processed_granules))
 
 
+def get_neighbors(granule, depth):
+    return []
+
+
 def get_payload_for_job(subscription, granule):
-    payload = subscription['job_specification']
-    if 'job_parameters' not in payload:
-        payload['job_parameters'] = {}
-    payload['job_parameters']['granules'] = [granule]
+    job_specification = subscription['job_specification']
+    if 'job_parameters' not in job_specification:
+        job_specification['job_parameters'] = {}
+
+    job_type = subscription['job_specification']['job_type']
+
+    if job_type in ['RTC_GAMMA']:
+        job_specification['job_parameters']['granules'] = [granule]
+        payload = [job_specification]
+    elif job_type in ['AUTORIFT', 'INSAR_GAMMA']:
+        payload = [deepcopy(job_specification), deepcopy(job_specification)]
+        neighbors = get_neighbors(granule, 2)
+        payload[0]['job_parameters']['granules'] = [granule, neighbors[0]]
+        payload[1]['job_parameters']['granules'] = [granule, neighbors[1]]
+    else:
+        raise ValueError(f'Subscription job type {job_type} not supported')
     return payload
 
 
 def submit_jobs_for_granule(subscription, granule):
-    payload = [
-        get_payload_for_job(subscription, granule)
-    ]
+    payload = get_payload_for_job(subscription, granule)
     dynamo.jobs.put_jobs(subscription['user_id'], payload)
 
 
@@ -49,4 +67,6 @@ def handle_subscription(subscription):
 def lambda_handler(event, context):
     subscriptions = dynamo.subscriptions.get_all_subscriptions()
     for subscription in subscriptions:
-        handle_subscription(subscription)
+        end_filter = datetime.now(tzinfo=timezone.utc) + timedelta(days = 5)
+        if end_filter <= dateutil.parser.parse(subscription['search_parameters']['end']):
+            handle_subscription(subscription)
