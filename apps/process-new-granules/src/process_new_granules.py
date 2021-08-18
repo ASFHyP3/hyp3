46ad5b8f1ddcdb0e50a5046962cdbc44ef223f40
Andrew Johnston <asjohnston@alaska.edu>
2021-08-18 10:39:09 -0800

    fix process_new_granules bug

--- apps/process-new-granules/src/process_new_granules.py
+++ apps/process-new-granules/src/process_new_granules.py
@@ -21,8 +21,7 @@ def get_unprocessed_granules(subscription):
         name=subscription['job_specification']['name'],
         job_type=subscription['job_specification']['job_type'],
     )
-    #  TODO This line could be made a lot clearer.
-    processed_granules = itertools.chain(*[job['job_parameters']['granules'][0] for job in processed_jobs])
+    processed_granules = [job['job_parameters']['granules'][0] for job in processed_jobs]
     return list(set(all_granules) - set(processed_granules))
 
 
