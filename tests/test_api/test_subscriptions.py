48377cf8b9d4965a3fd8634da5f16cc828f8028a
James Rine <Jlrine2@users.noreply.github.com>
2021-08-18 09:51:39 -0800

    Update tests/test_api/test_subscriptions.py
    Co-authored-by: Andrew Johnston <asjohnston@alaska.edu>

--- tests/test_api/test_subscriptions.py
+++ tests/test_api/test_subscriptions.py
@@ -337,15 +337,4 @@ def test_get_subscription_by_id(client, tables):
 
     response = client.get(SUBSCRIPTIONS_URI + '/140191ab-486b-4080-ab1b-3e2c40aab6b8')
 
-    assert response.json == {
-        'subscription_id': '140191ab-486b-4080-ab1b-3e2c40aab6b8',
-        'user_id': 'subscriptionsUser2',
-        'search_parameters': {
-            'start': '2020-01-01T00:00:00+00:00',
-            'end': '2020-01-01T00:00:00+00:00',
-        },
-        'job_specification': {
-            'job_type': 'RTC_GAMMA',
-            'name': 'SubscriptionName'
-        }
-    }
+    assert response.json == items[1]
