be563c8f0d22a50eccd66b0cd3ae35de50071d06
James <jlrine2@alaska.edu>
2021-08-18 10:58:30 -0800

    flake8

--- tests/test_api/test_api_spec.py
+++ tests/test_api/test_api_spec.py
@@ -1,6 +1,6 @@
 from http import HTTPStatus
 
-from test_api.conftest import AUTH_COOKIE, JOBS_URI, SUBSCRIPTIONS_URI,USER_URI, login
+from test_api.conftest import AUTH_COOKIE, JOBS_URI, SUBSCRIPTIONS_URI, USER_URI, login
 
 from hyp3_api import auth, routes
 
