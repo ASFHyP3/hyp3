10b8a8c795708b22f40b268ea9d98ce458120649
Andrew Johnston <asjohnston@alaska.edu>
2021-08-18 15:47:40 -0800

    add test_get_neighbors

--- tests/test_process_new_granules.py
+++ tests/test_process_new_granules.py
@@ -6,6 +6,35 @@ import pytest
 import process_new_granules
 
 
+def test_get_neighbors():
+    granule = asf_search.ASFProduct({'properties': {'sceneName': 'granule'}, 'geometry': {}}),
+
+    def mock(payload):
+        assert payload == granule
+        return asf_search.ASFSearchResults([
+            asf_search.ASFProduct({'properties': {'sceneName': 'S1A_A', 'temporalBaseline': -3}, 'geometry': {}}),
+            asf_search.ASFProduct({'properties': {'sceneName': 'S1B_B', 'temporalBaseline': -2}, 'geometry': {}}),
+            asf_search.ASFProduct({'properties': {'sceneName': 'S1A_C', 'temporalBaseline': -1}, 'geometry': {}}),
+            asf_search.ASFProduct({'properties': {'sceneName': 'S1B_D', 'temporalBaseline': 0}, 'geometry': {}}),
+            asf_search.ASFProduct({'properties': {'sceneName': 'S1A_E', 'temporalBaseline': 1}, 'geometry': {}}),
+            asf_search.ASFProduct({'properties': {'sceneName': 'S1B_F', 'temporalBaseline': 2}, 'geometry': {}}),
+            asf_search.ASFProduct({'properties': {'sceneName': 'S1A_G', 'temporalBaseline': 3}, 'geometry': {}}),
+        ])
+
+    with patch('asf_search.baseline_search.stack_from_product', mock):
+        neighbors = process_new_granules.get_neighbors(granule, 1, 'S1')
+        assert neighbors == ['S1A_C']
+
+        neighbors = process_new_granules.get_neighbors(granule, 2, 'S1')
+        assert neighbors == ['S1B_B', 'S1A_C']
+
+        neighbors = process_new_granules.get_neighbors(granule, 2, 'S1A')
+        assert neighbors == ['S1A_A', 'S1A_C']
+
+        neighbors = process_new_granules.get_neighbors(granule, 5, 'S1B')
+        assert neighbors == ['S1B_B']
+
+
 def test_get_jobs_for_granule():
     granule = asf_search.ASFProduct({'properties': {'sceneName': 'GranuleName'}, 'geometry': {}})
 
