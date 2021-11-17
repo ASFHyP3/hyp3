1c38e4538d9594b64b35cff0e7ae448a71841980
James Rine <jlrine2@alaska.edu>
2021-11-17 13:01:18 -0900

    add dynamic time period

--- apps/scale-cluster/src/scale_cluster.py
+++ apps/scale-cluster/src/scale_cluster.py
@@ -1,13 +1,20 @@
 import boto3
-import datetime
+from datetime import datetime, timezone
+
+def get_time_period():
+    today = datetime.now(tz=timezone.utc).date()
+    start = today.replace(day=1)
+    next_month = 1 if start.month == 12 else start.month + 1
+    end = start.replace(month=next_month)
+    return {
+        'Start': str(start),
+        'End': str(end)
+    }
 
 
 def get_ec2_spending_month_to_date():
     client = boto3.client('ce')
-    period = {
-        'Start': '2021-10-01', # FIXME: Make this dynamic
-        'End': '2021-11-01'
-    }
+    time_period = get_time_period()
     granularity = 'MONTHLY'
     _filter = {
         'Dimensions': {
@@ -15,9 +22,8 @@ def get_ec2_spending_month_to_date():
             'Values': ['Amazon Elastic Compute Cloud - Compute', 'EC2 - Other']
         }
     }
-
     metrics = ['UnblendedCost']
-    response = client.get_cost_and_usage(TimePeriod=period, Granularity=granularity, Filter=_filter, Metrics=metrics)
+    response = client.get_cost_and_usage(TimePeriod=time_period, Granularity=granularity, Filter=_filter, Metrics=metrics)
     return float(response['ResultsByTime'][0]['Total']['UnblendedCost']['Amount'])
 
 
