5829b27e6b1cb186da6110cdac44e05af44a35de
James Rine <jlrine2@alaska.edu>
2021-11-17 15:03:26 -0900

    add logic for switching cluster size

--- apps/scale-cluster/src/scale_cluster.py
+++ apps/scale-cluster/src/scale_cluster.py
@@ -1,6 +1,12 @@
+import calendar
+from datetime import date
+from os import environ
+
 import boto3
 import dateutil.relativedelta
-from datetime import date, datetime, timezone
+
+BATCH = boto3.client('batch')
+COST_EXPLORER = boto3.client('ce')
 
 
 def get_time_period(today: date):
@@ -13,8 +19,7 @@ def get_time_period(today: date):
 
 
 def get_ec2_spending_month_to_date():
-    client = boto3.client('ce')
-    time_period = get_time_period(datetime.now(tz=timezone.utc).date())
+    time_period = get_time_period(date.today())
     granularity = 'MONTHLY'
     _filter = {
         'Dimensions': {
@@ -23,9 +28,36 @@ def get_ec2_spending_month_to_date():
         }
     }
     metrics = ['UnblendedCost']
-    response = client.get_cost_and_usage(TimePeriod=time_period, Granularity=granularity, Filter=_filter, Metrics=metrics)
+    response = COST_EXPLORER.get_cost_and_usage(TimePeriod=time_period, Granularity=granularity, Filter=_filter,
+                                                Metrics=metrics)
     return float(response['ResultsByTime'][0]['Total']['UnblendedCost']['Amount'])
 
 
+def update_compute_cluster_size(max_vcpus):
+    compute_environment_arn = environ['COMPUTE_ENVIRONMENT_ARN']
+    print(f'Updating {compute_environment_arn} maxvCpus to {max_vcpus}')
+    BATCH.update_compute_environment(
+        computeEnvironment=compute_environment_arn,
+        computeResources={'maxvCpus': max_vcpus},
+    )
+
+
+def get_max_vcpus(today, budget, spending):
+    days_in_month = calendar.monthrange(today.year, today.month)[1]
+
+    target_spending = (budget / days_in_month) * today.day
+    max_spending_for_next_day = (target_spending - spending)
+
+    if max_spending_for_next_day >= 1000:
+        max_vcpus = 1600
+    else:
+        max_vcpus = 600
+    return max_vcpus
+
+
 def lambda_handler(event, context):
-    pass
+    today = date.today()
+    monthly_compute_budget = int(environ['MONTHLY_COMPUTE_BUDGET'])
+    current_spending = get_ec2_spending_month_to_date()
+    max_vcpus = get_max_vcpus(today, monthly_compute_budget, current_spending)
+    update_compute_cluster_size(max_vcpus)
--- tests/test_scale_cluster.py
+++ tests/test_scale_cluster.py
@@ -13,3 +13,13 @@ def test_get_time_period():
 
     result = scale_cluster.get_time_period(date(year=2020, month=2, day=29))
     assert result == {'Start': '2020-02-01', 'End': '2020-03-01'}
+
+
+def test_get_max_vcpus():
+    # monthly budget == 10,000
+    assert scale_cluster.get_max_vcpus(date(2020, 1, 1), 1000, 0) == 600
+    assert scale_cluster.get_max_vcpus(date(2020, 1, 31), 1000, 0) == 1600
+    assert scale_cluster.get_max_vcpus(date(2020, 1, 31), 999.99, 0) == 600
+    assert scale_cluster.get_max_vcpus(date(2020, 1, 31), 0, 0) == 600
+    assert scale_cluster.get_max_vcpus(date(2020, 2, 20), 29000, 19001) == 600
+    assert scale_cluster.get_max_vcpus(date(2020, 2, 20), 29000, 19000) == 1600
