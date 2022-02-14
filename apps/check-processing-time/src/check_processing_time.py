45569c8e73c925df3386c29c5ced3a667e3c0f68
Jake Herrmann <jtherrmann@alaska.edu>
2022-02-14 12:48:14 -0900

    Check processing time for failed results

--- apps/check-processing-time/src/check_processing_time.py
+++ apps/check-processing-time/src/check_processing_time.py
@@ -1,5 +1,14 @@
+import json
+
+
+def get_time_from_attempts(attempts):
+    attempts.sort(key=lambda attempt: attempt['StartedAt'])
+    final_attempt = attempts[-1]
+    return final_attempt['StoppedAt'] - final_attempt['StartedAt']
+
+
 def lambda_handler(event, context):
     results = event['processing_results']
-    results['Attempts'].sort(key=lambda attempt: attempt['StartedAt'])
-    final_attempt = results['Attempts'][-1]
-    return final_attempt['StoppedAt'] - final_attempt['StartedAt']
+    if 'Attempts' in results:
+        return get_time_from_attempts(results['Attempts'])
+    return get_time_from_attempts(json.loads(results['Cause'])['Attempts'])
--- tests/test_check_processing_time.py
+++ tests/test_check_processing_time.py
@@ -2,16 +2,32 @@ import check_processing_time
 
 
 def test_single_attempt():
-    event = {
-        'processing_results': {
-            'Attempts': [{'Container': {}, 'StartedAt': 1644423555778, 'StatusReason': '', 'StoppedAt': 1644425055200}]
-        }
-    }
-    response = check_processing_time.lambda_handler(event, None)
-    assert response == 1644425055200 - 1644423555778
+    attempts = [{'Container': {}, 'StartedAt': 1644423555778, 'StatusReason': '', 'StoppedAt': 1644425055200}]
+    result = check_processing_time.get_time_from_attempts(attempts)
+    assert result == 1644425055200 - 1644423555778
 
 
 def test_multiple_attempts():
+    attempts = [
+        {'Container': {}, 'StartedAt': 1644609403693, 'StatusReason': '', 'StoppedAt': 1644609919331},
+        {'Container': {}, 'StartedAt': 1644610107570, 'StatusReason': '', 'StoppedAt': 1644611472015}
+    ]
+    result = check_processing_time.get_time_from_attempts(attempts)
+    assert result == 1644611472015 - 1644610107570
+
+
+def test_unsorted_attempts():
+    # I'm not sure if the lambda would ever be given unsorted attempts, but it seems worth testing that it doesn't
+    # depend on the list to be sorted.
+    attempts = [
+        {'Container': {}, 'StartedAt': 1644610107570, 'StatusReason': '', 'StoppedAt': 1644611472015},
+        {'Container': {}, 'StartedAt': 1644609403693, 'StatusReason': '', 'StoppedAt': 1644609919331}
+    ]
+    result = check_processing_time.get_time_from_attempts(attempts)
+    assert result == 1644611472015 - 1644610107570
+
+
+def test_lambda_handler_with_normal_results():
     event = {
         'processing_results': {
             'Attempts': [
@@ -24,16 +40,15 @@ def test_multiple_attempts():
     assert response == 1644611472015 - 1644610107570
 
 
-def test_unsorted_attempts():
-    # I'm not sure if the lambda would ever be given unsorted attempts, but it seems worth testing that it doesn't
-    # depend on the list to be sorted.
+def test_lambda_handler_with_failed_results():
     event = {
         'processing_results': {
-            'Attempts': [
-                {'Container': {}, 'StartedAt': 1644610107570, 'StatusReason': '', 'StoppedAt': 1644611472015},
-                {'Container': {}, 'StartedAt': 1644609403693, 'StatusReason': '', 'StoppedAt': 1644609919331}
-            ]
+            'Error': 'States.TaskFailed',
+            'Cause': '{"Attempts": ['
+                     '{"Container": {}, "StartedAt": 1643834765893, "StatusReason": "", "StoppedAt": 1643834766455}, '
+                     '{"Container": {}, "StartedAt": 1643834888866, "StatusReason": "", "StoppedAt": 1643834889448}, '
+                     '{"Container": {}, "StartedAt": 1643834907858, "StatusReason": "", "StoppedAt": 1643834908466}]}'
         }
     }
     response = check_processing_time.lambda_handler(event, None)
-    assert response == 1644611472015 - 1644610107570
+    assert response == 1643834908466 - 1643834907858
