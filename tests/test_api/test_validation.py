46bed03319458277ad7d71e6f851c4ba679038aa
Joseph H Kennedy <me@jhkennedy.org>
2022-02-16 15:13:54 -0900

    Add tests for Landsat-9 scenes

--- tests/test_api/test_validation.py
+++ tests/test_api/test_validation.py
@@ -171,6 +171,9 @@ def test_is_third_party_granule():
     assert validation.is_third_party_granule('LC08_L1TP_009011_20200820_20200905_02_T1')
     assert validation.is_third_party_granule('LO08_L1GT_043001_20201106_20201110_02_T2')
     assert validation.is_third_party_granule('LT08_L1GT_041001_20200125_20200925_02_T2')
+    assert validation.is_third_party_granule('LC09_L1GT_215109_20220125_20220125_02_T2')
+    assert validation.is_third_party_granule('LO09_L1GT_215109_20220210_20220210_02_T2')
+    assert validation.is_third_party_granule('LT09_L1GT_215109_20220210_20220210_02_T2')
     assert not validation.is_third_party_granule('S1A_IW_SLC__1SSH_20150608T205059_20150608T205126_006287_0083E8_C4F0')
     assert not validation.is_third_party_granule('foo')
 
