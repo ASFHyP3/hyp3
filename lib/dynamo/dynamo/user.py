ec2bc5a105fad6ebb86d5dece1aa3e69f685c580
Jake Herrmann <jtherrmann@alaska.edu>
2022-05-13 09:48:11 -0800

    Check if user has max_jobs_per_month field

--- lib/dynamo/dynamo/user.py
+++ lib/dynamo/dynamo/user.py
@@ -4,14 +4,14 @@ from typing import Optional
 from dynamo.util import DYNAMODB_RESOURCE
 
 
-def get_user(user):
+def get_user(user_id: str) -> Optional[dict]:
     table = DYNAMODB_RESOURCE.Table(environ['USERS_TABLE_NAME'])
-    response = table.get_item(Key={'user_id': user})
+    response = table.get_item(Key={'user_id': user_id})
     return response.get('Item')
 
 
-def get_priority(user) -> Optional[int]:
-    user = get_user(user)
+def get_priority(user_id: str) -> Optional[int]:
+    user = get_user(user_id)
     if user:
         priority = user.get('priority')
     else:
@@ -19,9 +19,9 @@ def get_priority(user) -> Optional[int]:
     return priority
 
 
-def get_max_jobs_per_month(user) -> Optional[int]:
-    user = get_user(user)
-    if user:
+def get_max_jobs_per_month(user_id: str) -> Optional[int]:
+    user = get_user(user_id)
+    if user is not None and 'max_jobs_per_month' in user:
         max_jobs_per_month = user['max_jobs_per_month']
         if max_jobs_per_month is not None:
             max_jobs_per_month = int(max_jobs_per_month)
