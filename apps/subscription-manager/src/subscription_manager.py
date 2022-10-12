161d38bd14030f11436bb2dace5bc3d63395416c
Jake Herrmann <jtherrmann@alaska.edu>
2022-10-11 16:49:32 -0800

    Implement subscription lambda logging the right way

--- apps/subscription-manager/src/subscription_manager.py
+++ apps/subscription-manager/src/subscription_manager.py
@@ -1,9 +1,14 @@
 import json
+import logging
 import os
 
 import boto3
 
 import dynamo
+
+logging.basicConfig()
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 LAMBDA_CLIENT = boto3.client('lambda')
 
@@ -21,18 +26,18 @@ def invoke_worker(worker_function_arn: str, subscription: dict) -> dict:
 
 def lambda_handler(event, context):
     worker_function_arn = os.environ['SUBSCRIPTION_WORKER_ARN']
-    print(f'Worker function ARN: {worker_function_arn}')
+    logger.info(f'Worker function ARN: {worker_function_arn}')
 
     subscriptions = dynamo.subscriptions.get_all_subscriptions()
-    print(f'Got {len(subscriptions)} subscriptions')
+    logger.info(f'Got {len(subscriptions)} subscriptions')
 
     enabled_subscriptions = [subscription for subscription in subscriptions if subscription['enabled']]
-    print(f'Got {len(enabled_subscriptions)} enabled subscriptions')
+    logger.info(f'Got {len(enabled_subscriptions)} enabled subscriptions')
 
     for count, subscription in enumerate(enabled_subscriptions, start=1):
-        print(
+        logger.info(
             f'({count}/{len(enabled_subscriptions)}) '
             f'Invoking worker for subscription {subscription["subscription_id"]}'
         )
         response = invoke_worker(worker_function_arn, subscription)
-        print(f'Got response status code {response["StatusCode"]} for subscription {subscription["subscription_id"]}')
+        logger.info(f'Got response status code {response["StatusCode"]} for subscription {subscription["subscription_id"]}')
