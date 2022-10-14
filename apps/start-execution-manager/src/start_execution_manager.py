8507014fac4920aaa4f5c0af80bee7b0c7195473
Jake Herrmann <jtherrmann@alaska.edu>
2022-10-13 16:35:41 -0800

    Start 3 batches of 300 executions

--- apps/start-execution-manager/src/start_execution_manager.py
+++ apps/start-execution-manager/src/start_execution_manager.py
@@ -24,7 +24,7 @@ def invoke_worker(worker_function_arn: str, jobs: list[dict]) -> dict:
 # TODO add logging
 def lambda_handler(event, context):
     worker_function_arn = os.environ['SUBSCRIPTION_WORKER_ARN']  # TODO add this in cf
-    pending_jobs = dynamo.jobs.get_jobs_waiting_for_execution(limit=400)  # TODO increase this limit, or just remove it?
-    batch_size = 500  # TODO decide on appropriate value
+    pending_jobs = dynamo.jobs.get_jobs_waiting_for_execution(limit=900)
+    batch_size = 300
     for i in range(0, len(pending_jobs), batch_size):
         invoke_worker(worker_function_arn, pending_jobs[i:i+batch_size])
