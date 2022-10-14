144f2f1f0cc38c14dc0e65d82b77cfe5e4d41fcd
Jake Herrmann <jtherrmann@alaska.edu>
2022-10-13 16:18:11 -0800

    Update start_execution_worker tests

--- apps/start-execution-worker/src/start_execution_worker.py
+++ apps/start-execution-worker/src/start_execution_worker.py
@@ -1,5 +1,5 @@
 import json
-from os import environ
+import os
 
 import boto3
 
@@ -17,10 +17,13 @@ def convert_parameters_to_strings(parameters):
 
 
 def submit_jobs(jobs):
+    step_function_arn = os.environ['STEP_FUNCTION_ARN']
     for job in jobs:
+        # Convert parameters to strings so they can be passed to Batch; see:
+        # https://docs.aws.amazon.com/batch/latest/APIReference/API_SubmitJob.html#Batch-SubmitJob-request-parameters
         job['job_parameters'] = convert_parameters_to_strings(job['job_parameters'])
         STEP_FUNCTION.start_execution(
-            stateMachineArn=environ['STEP_FUNCTION_ARN'],
+            stateMachineArn=step_function_arn,
             input=json.dumps(job, sort_keys=True),
             name=job['job_id'],
         )
