31f750e0651be23b9047894995c9af0337a9ff53
Joseph H Kennedy <me@jhkennedy.org>
2023-03-16 17:29:42 -0800

    rework jobspec for hyp3 assumptions

--- apps/api/src/hyp3_api/validation.py
+++ apps/api/src/hyp3_api/validation.py
@@ -66,7 +66,7 @@ def get_cmr_metadata(granules):
 
 
 def is_third_party_granule(granule):
-    return granule.startswith('S2') or granule.startswith('L')
+    return granule.startswith('S2') or granule.startswith('L') or granule.startswith('/vsicurl')
 
 
 def check_granules_exist(granules, granule_metadata):
