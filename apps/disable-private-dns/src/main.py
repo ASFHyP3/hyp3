834350dc76e95a61e24c103f44b1b6e2a8d9c309
Forrest Williams <ffwilliams2@alaska.edu>
2023-11-16 08:33:02 -0600

    make sure options is DISabled

--- apps/disable-private-dns/src/main.py
+++ apps/disable-private-dns/src/main.py
@@ -12,19 +12,19 @@ def get_endpoint(endpoint_name):
             return endpoint
 
 
-def set_private_dns_enabled(endpoint_id):
-    response = CLIENT.modify_vpc_endpoint(VpcEndpointId=endpoint_id, PrivateDnsEnabled=True)
-    print(f"Private DNS enabled for VPC Endpoint: {endpoint_id}.")
+def set_private_dns_disabled(endpoint_id):
+    response = CLIENT.modify_vpc_endpoint(VpcEndpointId=endpoint_id, PrivateDnsEnabled=False)
+    print(f"Private DNS disabled for VPC Endpoint: {endpoint_id}.")
     return response
 
 
 def enable_private_dns(endpoint_name):
     endpoint = get_endpoint(endpoint_name)
     if not endpoint['PrivateDnsEnabled']:
-        print(f"Private DNS not enabled for VPC Endpoint: {endpoint['VpcEndpointId']}, changing...")
-        set_private_dns_enabled(endpoint['VpcEndpointId'])
+        print(f"Private DNS enabled for VPC Endpoint: {endpoint['VpcEndpointId']}, changing...")
+        set_private_dns_disabled(endpoint['VpcEndpointId'])
     else:
-        print(f"Private already enabled for VPC Endpoint: {endpoint['VpcEndpointId']}, doing nothing.")
+        print(f"Private already disabled for VPC Endpoint: {endpoint['VpcEndpointId']}, doing nothing.")
 
 
 def lambda_handler(event, context):
