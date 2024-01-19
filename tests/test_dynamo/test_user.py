85edf3b52dc310719a6c9a6fee22850537acfb69
Jake Herrmann <jtherrmann@alaska.edu>
2024-01-19 10:10:30 -0900

    delete obsolete dynamo.user tests

--- tests/test_dynamo/test_user.py
+++ tests/test_dynamo/test_user.py
@@ -1,6 +1,7 @@
 import dynamo
 
 
+# TODO update these field names?
 def test_get_user(tables):
     table_items = [
         {
@@ -18,32 +19,3 @@ def test_get_user(tables):
     assert dynamo.user.get_user('user1') == table_items[0]
     assert dynamo.user.get_user('user2') == table_items[1]
     assert dynamo.user.get_user('foo') is None
-
-
-def test_get_max_jobs_per_month(tables, monkeypatch):
-    monkeypatch.setenv('MONTHLY_JOB_QUOTA_PER_USER', '5')
-    assert dynamo.user.get_max_jobs_per_month('user1') == 5
-
-    user = {'user_id': 'user1'}
-    tables.users_table.put_item(Item=user)
-    assert dynamo.user.get_max_jobs_per_month('user1') == 5
-
-    user['max_jobs_per_month'] = 10
-    tables.users_table.put_item(Item=user)
-    assert dynamo.user.get_max_jobs_per_month('user1') == 10
-
-    user['max_jobs_per_month'] = None
-    tables.users_table.put_item(Item=user)
-    assert dynamo.user.get_max_jobs_per_month('user1') is None
-
-
-def test_get_priority(tables):
-    assert dynamo.user.get_priority('user1') is None
-
-    user = {'user_id': 'user1'}
-    tables.users_table.put_item(Item=user)
-    assert dynamo.user.get_priority('user1') is None
-
-    user['priority'] = 10
-    tables.users_table.put_item(Item=user)
-    assert dynamo.user.get_priority('user1') == 10
