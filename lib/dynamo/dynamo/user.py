96a8ba1e9c5def9db35086931efd034dcf7de4cb
Jake Herrmann <jtherrmann@alaska.edu>
2024-01-24 11:26:22 -0900

    revise dynamo.user functions

--- lib/dynamo/dynamo/user.py
+++ lib/dynamo/dynamo/user.py
@@ -2,35 +2,86 @@ import os
 from datetime import datetime, timezone
 from decimal import Decimal
 from os import environ
-from typing import Optional
 
 import botocore.exceptions
 
 from dynamo.util import DYNAMODB_RESOURCE
 
 
-def get_user(user_id: str) -> Optional[dict]:
-    table = DYNAMODB_RESOURCE.Table(environ['USERS_TABLE_NAME'])
-    response = table.get_item(Key={'user_id': user_id})
-    return response.get('Item')
+class DatabaseConditionException(Exception):
+    """Raised when a DynamoDB condition expression check fails."""
 
 
-def create_user(user_id: str) -> dict:
-    table = DYNAMODB_RESOURCE.Table(environ['USERS_TABLE_NAME'])
-    if get_user(user_id):
-        raise ValueError(f'user {user_id} already exists')
-    user = {'user_id': user_id, 'remaining_credits': Decimal(os.environ['DEFAULT_CREDITS_PER_USER'])}
-    table.put_item(Item=user)
+# TODO tests
+def get_or_create_user(user_id: str) -> dict:
+    current_month = datetime.now(tz=timezone.utc).strftime('%Y-%m')
+    default_credits = Decimal(os.environ['DEFAULT_CREDITS_PER_USER'])
+
+    users_table = DYNAMODB_RESOURCE.Table(environ['USERS_TABLE_NAME'])
+    user = users_table.get_item(Key={'user_id': user_id}).get('Item')
+
+    if user is not None:
+        _reset_credits_if_needed(
+            user=user,
+            default_credits=default_credits,
+            current_month=current_month,
+            users_table=users_table,
+        )
+    else:
+        user = _create_user(
+            user_id=user_id,
+            default_credits=default_credits,
+            current_month=current_month,
+            users_table=users_table,
+        )
     return user
 
 
-# TODO unit tests
-def decrement_credits(user_id: str, cost: float) -> None:
-    if cost < 0:
-        raise ValueError(f'Cost {cost} < 0')
-    table = DYNAMODB_RESOURCE.Table(environ['USERS_TABLE_NAME'])
+# TODO tests
+def _create_user(user_id: str, default_credits: Decimal, current_month: str, users_table) -> dict:
+    user = {'user_id': user_id, 'remaining_credits': default_credits, 'month_last_reset': current_month}
     try:
-        table.update_item(
+        users_table.put_item(Item=user, ConditionExpression='attribute_not_exists(user_id)')
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] == 'ConditionalCheckFailedException':
+            raise DatabaseConditionException(f'Failed to create user {user_id}')
+        raise
+    return user
+
+
+# TODO tests
+def _reset_credits_if_needed(user: dict, default_credits: Decimal, current_month: str, users_table) -> None:
+    if os.environ['MONTHLY_CREDITS_RESET'] == 'yes'\
+            and user['month_last_reset'] < current_month\
+            and user['remaining_credits'] is not None:
+        user_id = user['user_id']
+        try:
+            users_table.update_item(
+                Key={'user_id': user_id},
+                UpdateExpression='SET month_last_reset = :current_month,'
+                                 ' remaining_credits = :default_credits',
+                ConditionExpression='month_last_reset < :current_month'
+                                    ' AND attribute_type(remaining_credits, :number)',
+                ExpressionAttributeValues={
+                    ':default_credits': default_credits,
+                    ':current_month': current_month,
+                    ':number': 'N',
+                },
+            )
+        except botocore.exceptions.ClientError as e:
+            if e.response['Error']['Code'] == 'ConditionalCheckFailedException':
+                raise DatabaseConditionException(f'Failed to perform monthly credits reset for user {user_id}')
+            raise
+
+
+# TODO tests
+# TODO fine to not call _reset_credits_if_needed?
+def decrement_credits(user_id: str, cost: float) -> None:
+    if cost <= 0:
+        raise ValueError(f'Cost {cost} <= 0')
+    users_table = DYNAMODB_RESOURCE.Table(environ['USERS_TABLE_NAME'])
+    try:
+        users_table.update_item(
             Key={'user_id': user_id},
             UpdateExpression='ADD remaining_credits :delta',
             ConditionExpression='remaining_credits >= :cost',
@@ -38,38 +89,5 @@ def decrement_credits(user_id: str, cost: float) -> None:
         )
     except botocore.exceptions.ClientError as e:
         if e.response['Error']['Code'] == 'ConditionalCheckFailedException':
-            raise ValueError(
-                f"Unable to update remaining_credits for user_id '{user_id}'."
-                f' It is likely that the user record does not exist, remaining_credits < {cost},'
-                ' or remaining_credits is of the wrong data type.'
-            )
+            raise DatabaseConditionException(f'Failed to decrement credits for user {user_id}')
         raise
-
-
-# TODO tests
-# TODO call this function wherever we interact with user records
-# TODO update the last_updated field in create_user or just call this function
-# TODO make last_updated be a full timestamp (as seconds since epoch or datetime str)?
-# TODO don't update if remaining_credits is null
-# TODO race conditions?
-def reset_credits(user_id: str) -> None:
-    if os.environ['MONTHLY_CREDITS_RESET'] == 'yes':
-        table = DYNAMODB_RESOURCE.Table(environ['USERS_TABLE_NAME'])
-        default_credits = os.environ['DEFAULT_CREDITS_PER_USER']
-        current_month = datetime.now(tz=timezone.utc).strftime('%Y-%m')
-        user = table.get_item(Key={'user_id': user_id})['Item']
-        if user['last_updated'] < current_month:
-            try:
-                table.update_item(
-                    Key={'user_id': user_id},
-                    UpdateExpression='SET remaining_credits = :default_credits',
-                    ConditionExpression='last_updated < :current_month',
-                    ExpressionAttributeValues={
-                        ':default_credits': Decimal(default_credits),
-                        ':current_month': current_month,
-                    },
-                )
-            except botocore.exceptions.ClientError as e:
-                if e.response['Error']['Code'] == 'ConditionalCheckFailedException':
-                    raise ValueError('Unable to update user record')  # TODO better error message
-                raise
