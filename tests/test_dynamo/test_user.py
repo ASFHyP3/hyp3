337f5aae8c4b9fac97248199081a0175e76d489a
Jake Herrmann <jtherrmann@alaska.edu>
2024-01-25 15:44:12 -0900

    remove old test decrement credits

--- tests/test_dynamo/test_user.py
+++ tests/test_dynamo/test_user.py
@@ -178,32 +178,3 @@ def test_reset_credits_failed_infinite_credits(tables, monkeypatch):
             users_table=tables.users_table,
         )
 
-
-# TODO update
-def test_decrement_credits(tables):
-    with pytest.raises(ValueError):
-        dynamo.user.decrement_credits('foo', 1)
-
-    user = {'user_id': 'foo', 'remaining_credits': 25}
-    tables.users_table.put_item(Item=user)
-
-    with pytest.raises(ValueError):
-        dynamo.user.decrement_credits('foo', -1)
-
-    dynamo.user.decrement_credits('foo', 1)
-    response = tables.users_table.scan()
-    assert response['Items'] == [{'user_id': 'foo', 'remaining_credits': 24}]
-
-    dynamo.user.decrement_credits('foo', 4)
-    response = tables.users_table.scan()
-    assert response['Items'] == [{'user_id': 'foo', 'remaining_credits': 20}]
-
-    # TODO should this case set remaining_credits to zero?
-    with pytest.raises(ValueError):
-        dynamo.user.decrement_credits('foo', 21)
-    response = tables.users_table.scan()
-    assert response['Items'] == [{'user_id': 'foo', 'remaining_credits': 20}]
-
-    dynamo.user.decrement_credits('foo', 20)
-    response = tables.users_table.scan()
-    assert response['Items'] == [{'user_id': 'foo', 'remaining_credits': 0}]
