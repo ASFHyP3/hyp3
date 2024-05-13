7fd8695a3887c7fc1baf844ed59b6bf5302aee87
Jake Herrmann <jtherrmann@alaska.edu>
2024-05-13 15:41:07 -0800

    do not print wsgi response

--- apps/api/src/hyp3_api/lambda_handler.py
+++ apps/api/src/hyp3_api/lambda_handler.py
@@ -7,6 +7,4 @@ serverless_wsgi.TEXT_MIME_TYPES.append('application/problem+json')
 
 
 def handler(event, context):
-    response = serverless_wsgi.handle_request(app, event, context)
-    print(response)
-    return response
+    return serverless_wsgi.handle_request(app, event, context)
