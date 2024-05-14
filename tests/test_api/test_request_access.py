8e56b35fdfce7c94a2a7b586ea8eabc40c82704d
Jake Herrmann <jtherrmann@alaska.edu>
2024-05-14 15:34:16 -0800

    update test

--- tests/test_api/test_request_access.py
+++ tests/test_api/test_request_access.py
@@ -1,11 +1,18 @@
+import unittest.mock
 from http import HTTPStatus
 
-from test_api.conftest import REQUEST_ACCESS_URI, login
+from test_api.conftest import DEFAULT_ACCESS_TOKEN, REQUEST_ACCESS_URI, login
 
 
 def test_request_access(client):
     login(client, 'foo')
 
-    response = client.get(REQUEST_ACCESS_URI)
+    with unittest.mock.patch('dynamo.user.get_edl_profile') as mock_get_edl_profile:
+        mock_get_edl_profile.return_value = {'email_address': 'foo@example.com'}
+        response = client.get(REQUEST_ACCESS_URI)
+        mock_get_edl_profile.assert_called_once_with('foo', DEFAULT_ACCESS_TOKEN)
+
     assert response.status_code == HTTPStatus.OK
-    assert b'TODO: intro text' in response.data
+    assert b'Request Access to ASF On Demand Processing' in response.data
+    assert b'<b>Username:</b> foo' in response.data
+    assert b'<b>Email Address:</b> foo@example.com' in response.data
