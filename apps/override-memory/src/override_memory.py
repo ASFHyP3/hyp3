b44a607a741badc72489ce6ffc203c1de5d92f94
Jake Herrmann <jtherrmann@alaska.edu>
2024-09-05 14:46:35 -0800

    test override_memory

--- apps/override-memory/src/override_memory.py
+++ apps/override-memory/src/override_memory.py
@@ -1,3 +1,9 @@
+AUTORIFT_S2_MEMORY = 7875
+AUTORIFT_LANDSAT_MEMORY = 15750
+RTC_GAMMA_10M_MEMORY = 63200
+WATER_MAP_10M_MEMORY = 126000
+
+
 def get_resource_requirements(memory: int) -> dict:
     return {
         'ResourceRequirements': [
@@ -13,15 +19,15 @@ def lambda_handler(event: dict, _) -> dict:
     job_type, job_parameters = event['job_type'], event['job_parameters']
 
     if job_type == 'AUTORIFT' and job_parameters['granules'][0].startswith('S2'):
-        return get_resource_requirements(7875)
+        return get_resource_requirements(AUTORIFT_S2_MEMORY)
 
     if job_type == 'AUTORIFT' and job_parameters['granules'][0].startswith('L'):
-        return get_resource_requirements(15750)
+        return get_resource_requirements(AUTORIFT_LANDSAT_MEMORY)
 
     if job_type == 'RTC_GAMMA' and job_parameters['resolution'] in ['10', '20']:
-        return get_resource_requirements(63200)
+        return get_resource_requirements(RTC_GAMMA_10M_MEMORY)
 
     if job_type in ['WATER_MAP', 'WATER_MAP_EQ'] and job_parameters['resolution'] in ['10', '20']:
-        return get_resource_requirements(126000)
+        return get_resource_requirements(WATER_MAP_10M_MEMORY)
 
     return {}
--- tests/test_override_memory.py
+++ tests/test_override_memory.py
@@ -1,2 +1,138 @@
-def test_override_memory():
-    raise NotImplementedError()
+from override_memory import (
+    AUTORIFT_S2_MEMORY,
+    AUTORIFT_LANDSAT_MEMORY,
+    RTC_GAMMA_10M_MEMORY,
+    WATER_MAP_10M_MEMORY,
+    lambda_handler,
+)
+
+
+def test_override_memory_default():
+    assert lambda_handler(
+        {
+            'job_type': 'foo',
+            'job_parameters': {},
+        },
+        None,
+    ) == {}
+
+
+def test_override_memory_autorift():
+    assert lambda_handler(
+        {
+            'job_type': 'AUTORIFT',
+            'job_parameters': {'granules': ['S2B_MSIL1C_20200105T152259_N0208_R039_T13CES_20200105T181230']},
+        },
+        None,
+    ) == {
+        'ResourceRequirements': [
+            {
+                'Type': 'MEMORY',
+                'Value': AUTORIFT_S2_MEMORY,
+            }
+        ]
+    }
+    assert lambda_handler(
+        {
+            'job_type': 'AUTORIFT',
+            'job_parameters': {'granules': ['LC08_L1GT_118112_20210107_20210107_02_T2']},
+        },
+        None,
+    ) == {
+        'ResourceRequirements': [
+            {
+                'Type': 'MEMORY',
+                'Value': AUTORIFT_LANDSAT_MEMORY,
+            }
+        ]
+    }
+
+
+def test_override_memory_rtc_gamma():
+    assert lambda_handler(
+        {
+            'job_type': 'RTC_GAMMA',
+            'job_parameters': {'resolution': '10'},
+        },
+        None,
+    ) == {
+        'ResourceRequirements': [
+            {
+                'Type': 'MEMORY',
+                'Value': RTC_GAMMA_10M_MEMORY,
+            }
+        ]
+    }
+    assert lambda_handler(
+        {
+            'job_type': 'RTC_GAMMA',
+            'job_parameters': {'resolution': '20'},
+        },
+        None,
+    ) == {
+        'ResourceRequirements': [
+            {
+                'Type': 'MEMORY',
+                'Value': RTC_GAMMA_10M_MEMORY,
+            }
+        ]
+    }
+
+
+def test_override_memory_water_map():
+    assert lambda_handler(
+        {
+            'job_type': 'WATER_MAP',
+            'job_parameters': {'resolution': '10'},
+        },
+        None,
+    ) == {
+        'ResourceRequirements': [
+            {
+                'Type': 'MEMORY',
+                'Value': WATER_MAP_10M_MEMORY,
+            }
+        ]
+    }
+    assert lambda_handler(
+        {
+            'job_type': 'WATER_MAP',
+            'job_parameters': {'resolution': '20'},
+        },
+        None,
+    ) == {
+        'ResourceRequirements': [
+            {
+                'Type': 'MEMORY',
+                'Value': WATER_MAP_10M_MEMORY,
+            }
+        ]
+    }
+    assert lambda_handler(
+        {
+            'job_type': 'WATER_MAP_EQ',
+            'job_parameters': {'resolution': '10'},
+        },
+        None,
+    ) == {
+        'ResourceRequirements': [
+            {
+                'Type': 'MEMORY',
+                'Value': WATER_MAP_10M_MEMORY,
+            }
+        ]
+    }
+    assert lambda_handler(
+        {
+            'job_type': 'WATER_MAP_EQ',
+            'job_parameters': {'resolution': '20'},
+        },
+        None,
+    ) == {
+        'ResourceRequirements': [
+            {
+                'Type': 'MEMORY',
+                'Value': WATER_MAP_10M_MEMORY,
+            }
+        ]
+    }
