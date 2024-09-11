daf6368ec8362607de017361172643c6e28f553f
Jake Herrmann <jtherrmann@alaska.edu>
2024-09-11 09:57:20 -0800

    memory constants

--- apps/set-batch-overrides/src/set_batch_overrides.py
+++ apps/set-batch-overrides/src/set_batch_overrides.py
@@ -2,6 +2,11 @@ AUTORIFT_S2_MEMORY = '7875'
 AUTORIFT_LANDSAT_MEMORY = '15750'
 RTC_GAMMA_10M_MEMORY = '63200'
 WATER_MAP_10M_MEMORY = '126000'
+INSAR_ISCE_BURST_MEMORY_8G = '7500'
+INSAR_ISCE_BURST_MEMORY_16G = '15500'
+INSAR_ISCE_BURST_MEMORY_32G = '31500'
+INSAR_ISCE_BURST_MEMORY_64G = '63500'
+INSAR_ISCE_BURST_MEMORY_128G = '127500'
 
 
 def get_resource_requirements(memory: str) -> dict:
@@ -16,35 +21,31 @@ def get_resource_requirements(memory: str) -> dict:
 
 
 def get_insar_isce_burst_memory(job_parameters: dict) -> str:
-    # TODO:
-    # - refine based on more data
-    # - refactor lookup structure and/or pull out constants?
-    # - can we slightly reduce the 500M padding in order to bump up some of the intervals?
     bursts = len(job_parameters['reference'].split(' '))
     looks = job_parameters['looks']
     if looks == '5x1':
         if bursts < 2:
-            return '7500'
+            return INSAR_ISCE_BURST_MEMORY_8G
         if bursts < 4:
-            return '15500'
+            return INSAR_ISCE_BURST_MEMORY_16G
         if bursts < 11:
-            return '31500'
+            return INSAR_ISCE_BURST_MEMORY_32G
         if bursts < 25:
-            return '63500'
+            return INSAR_ISCE_BURST_MEMORY_64G
         if bursts < 31:
-            return '127500'
+            return INSAR_ISCE_BURST_MEMORY_128G
     if looks == '10x2':
         if bursts < 10:
-            return '7500'
+            return INSAR_ISCE_BURST_MEMORY_8G
         if bursts < 21:
-            return '15500'
+            return INSAR_ISCE_BURST_MEMORY_16G
         if bursts < 31:
-            return '31500'
+            return INSAR_ISCE_BURST_MEMORY_32G
     if looks == '20x4':
         if bursts < 23:
-            return '7500'
+            return INSAR_ISCE_BURST_MEMORY_8G
         if bursts < 31:
-            return '15500'
+            return INSAR_ISCE_BURST_MEMORY_16G
     raise ValueError(f'No memory value for {bursts} bursts and {looks} looks')
 
 
