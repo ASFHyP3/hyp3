6ff153f0bf38b23126fd22aa86c05dbb64540883
Jake Herrmann <jtherrmann@alaska.edu>
2024-10-03 16:38:01 -0800

    render map state for step function

--- apps/render_cf.py
+++ apps/render_cf.py
@@ -20,70 +20,111 @@ def get_steps_for_jobs(job_types: dict) -> dict:
 
 def get_steps_for_job(job_spec: dict) -> dict:
     steps = {}
-    tasks = job_spec["tasks"]
+    tasks = job_spec['tasks']
     for i in range(len(tasks)):
         task = tasks[i]
-        next_step_name = tasks[i + 1]["name"] if i < len(tasks) - 1 else "GET_FILES"
-        steps[task["name"]] = get_step_for_task(task, i, next_step_name, job_spec)
+        next_step_name = tasks[i + 1]['name'] if i < len(tasks) - 1 else 'GET_FILES'
+        steps[task['name']] = get_step_for_task(task, i, next_step_name, job_spec)
     return steps
 
 
 def get_step_for_task(task: dict, index: int, next_step_name: str, job_spec: dict) -> dict:
-    if "map" in task:
-        return get_step_for_map_task(task, index, next_step_name, job_spec)
-    return get_step_for_normal_task(task, index, next_step_name, job_spec)
+    if 'map' in task:
+        step = get_step_for_map_task(task, job_spec)
+    else:
+        step = get_step_for_batch_submit_job(task, job_spec)
+    step.update(
+        {
+            'Catch': [
+                {
+                    'ErrorEquals': [
+                        'States.ALL'
+                    ],
+                    'ResultPath': f'$.results.processing_results.step_{index}',
+                    'Next': 'PROCESSING_FAILED',
+                },
+            ],
+            'ResultPath': f'$.results.processing_results.step_{index}',
+            'Next': next_step_name,
+        }
+    )
+    return step
 
 
-def get_step_for_normal_task(task: dict, index: int, next_step_name: str, job_spec: dict) -> dict:
-    compute_environment = job_spec["compute_environment"]["name"]
-    job_queue = "JobQueueArn" if compute_environment == "Default" else compute_environment + "JobQueueArn"
+def get_step_for_map_task(task: dict, job_spec: dict) -> dict:
+    item, items = parse_task_map(task['map'])
+    job_parameters = get_job_parameters(item, items, job_spec)
+    submit_job_step = get_step_for_batch_submit_job(task, job_spec)
+    submit_job_step['End'] = True
     return {
-        "Type": "Task",
-        "Resource": "arn:aws:states:::batch:submitJob.sync",
-        "Parameters": {
-            "JobDefinition": "${"+ snake_to_pascal_case(task["name"]) + "}",
-            "JobName.$": "$.job_id",
-            "JobQueue": "${" + job_queue + "}",
-            "ShareIdentifier": "default",
-            "SchedulingPriorityOverride.$": "$.priority",
-            "Parameters.$": "$.job_parameters",
-            "ContainerOverrides.$": "$.container_overrides",
-            "RetryStrategy": {
-                "Attempts": 3
-            },
+        'Type': 'Map',
+        'ItemsPath': f'$.job_parameters.{items}',
+        'ItemSelector': {
+            'job_id.$': '$.job_id',
+            'priority.$': '$.priority',
+            'container_overrides.$': '$.container_overrides',
+            'job_parameters': job_parameters,
         },
-        "ResultPath": f"$.results.processing_results.step_{index}",
-        "Next": next_step_name,
-        "Retry": [
-            {
-                "ErrorEquals": [
-                    "Batch.ServerException",
-                    "Batch.AWSBatchException"
-                ],
-                "MaxAttempts": 2
-            },
-            {
-                "ErrorEquals": [
-                    "States.ALL"
-                ],
-                "MaxAttempts": 0
+        'ItemProcessor': {
+            'StartAt': task['name'],
+            'States': {
+                task['name']: submit_job_step,
             }
-        ],
-        "Catch": [
-            {
-                "ErrorEquals": [
-                    "States.ALL"
-                ],
-                "Next": "PROCESSING_FAILED",
-                "ResultPath": f"$.results.processing_results.step_{index}"
-            }
-        ],
+        }
     }
 
 
-def get_step_for_map_task(task: dict, index: int, next_step_name: str, job_spec: dict) -> dict:
-    # TODO
-    pass
+def parse_task_map(task_map: str) -> tuple[str, str]:
+    tokens = task_map.split(' ')
+    assert len(tokens) == 4
+    assert tokens[0], tokens[2] == ('for', 'in')
+    return tokens[1], tokens[3]
+
+
+def get_job_parameters(item: str, items: str, job_spec: dict) -> dict:
+    job_parameters = {
+        f'{param}.$': f'$.job_parameters.{param}'
+        for param in job_spec['parameters']
+        if param != items
+    }
+    job_parameters[f'{item}.$'] = '$$.Map.Item.Value'
+    return job_parameters
+
+
+def get_step_for_batch_submit_job(task: dict, job_spec: dict) -> dict:
+    compute_environment = job_spec['compute_environment']['name']
+    job_queue = 'JobQueueArn' if compute_environment == 'Default' else compute_environment + 'JobQueueArn'
+    return {
+        'Type': 'Task',
+        'Resource': 'arn:aws:states:::batch:submitJob.sync',
+        'Parameters': {
+            'JobDefinition': '${'+ snake_to_pascal_case(task['name']) + '}',
+            'JobName.$': '$.job_id',
+            'JobQueue': '${' + job_queue + '}',
+            'ShareIdentifier': 'default',
+            'SchedulingPriorityOverride.$': '$.priority',
+            'Parameters.$': '$.job_parameters',
+            'ContainerOverrides.$': '$.container_overrides',
+            'RetryStrategy': {
+                'Attempts': 3
+            },
+        },
+        'Retry': [
+            {
+                'ErrorEquals': [
+                    'Batch.ServerException',
+                    'Batch.AWSBatchException'
+                ],
+                'MaxAttempts': 2
+            },
+            {
+                'ErrorEquals': [
+                    'States.ALL'
+                ],
+                'MaxAttempts': 0
+            }
+        ]
+    }
 
 
 def render_templates(job_types, security_environment, api_name):
