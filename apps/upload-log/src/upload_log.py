124e8fdf8444e676f14ed977caec63a934062fd4
Joseph H Kennedy <me@jhkennedy.org>
2024-10-23 16:51:31 -0800

    write filed batch attempts with last log line for each attempt

--- apps/upload-log/src/upload_log.py
+++ apps/upload-log/src/upload_log.py
@@ -1,6 +1,5 @@
 import json
 from os import environ
-from typing import Optional
 
 import boto3
 from botocore.config import Config
@@ -8,12 +7,6 @@ from botocore.config import Config
 config = Config(retries={'max_attempts': 2, 'mode': 'standard'})
 CLOUDWATCH = boto3.client('logs', config=config)
 S3 = boto3.client('s3')
-
-
-def get_log_stream(result: dict) -> Optional[str]:
-    if 'Error' in result:
-        result = json.loads(result['Cause'])
-    return result['Container'].get('LogStreamName')
 
 
 def get_log_content(log_group, log_stream):
@@ -30,22 +23,9 @@ def get_log_content(log_group, log_stream):
     return '\n'.join(messages)
 
 
-def get_log_content_from_failed_attempts(cause: dict) -> str:
-    status = cause['Status']
-    assert status == 'FAILED', status
-
-    attempts = cause['Attempts']
-    if len(attempts) > 0:
-        content = '\n'.join(attempt['Container']['Reason'] for attempt in attempts)
-    else:
-        content = cause['StatusReason']
-
-    return content
-
-
-def write_log_to_s3(bucket, prefix, content):
-    key = f'{prefix}/{prefix}.log'
-    S3.put_object(Bucket=bucket, Key=key, Body=content, ContentType='text/plain')
+def write_log_to_s3(bucket, prefix, name, content, content_type: str = 'text/plain'):
+    key = f'{prefix}/{name}'
+    S3.put_object(Bucket=bucket, Key=key, Body=content, ContentType=content_type)
     tag_set = {
         'TagSet': [
             {
@@ -57,22 +37,50 @@ def write_log_to_s3(bucket, prefix, content):
     S3.put_object_tagging(Bucket=bucket, Key=key, Tagging=tag_set)
 
 
+def get_last_log_event(log_stream):
+    try:
+        response = CLOUDWATCH.describe_log_streams(logGroupName='/aws/batch/job', logStreamNamePrefix=log_stream)
+        last_event_time = response['logStreams'][0]['lastEventTimestamp']
+
+        log_query = {
+            'logGroupName': '/aws/batch/job',
+            'logStreamName': log_stream,
+            'startTime': last_event_time - 1_000,  # milliseconds since 1970-01-01T00:00:00Z
+            'endTime': last_event_time  + 1_000,  # milliseconds since 1970-01-01T00:00:00Z
+        }
+        response = CLOUDWATCH.get_log_events(**log_query)
+        return response['events'][-1]['message']
+    except:
+        return None
+
+
 def lambda_handler(event, context):
-    results_dict = event['processing_results']
-    result = results_dict[max(results_dict.keys())]
+    bucket = environ['BUCKET']
+    prefix = event['prefix']
 
-    log_content = None
+    _, last_result = max(event['processing_results'].items(), key=lambda x: x[0])
+    assert 'Error' in last_result
 
-    log_stream = get_log_stream(result)
-    if log_stream is not None:
+    cause = json.loads(last_result['Cause'])
+
+    for attempt in cause['Attempts']:
+        log_stream = attempt['Container'].get('LogStreamName')
+        if log_stream:
+            attempt['LastLogEvent'] = get_last_log_event(log_stream)
+        else:
+            attempt['LastLogEvent'] = None
+
+    write_log_to_s3(bucket, prefix, f'{prefix}.json', json.dumps(cause), content_type='application/json')
+
+    last_log_content = None
+
+    last_log_stream = cause['Attempts'][-1].get('LogStreamName')
+    if last_log_stream:
         try:
-            log_content = get_log_content(event['log_group'], log_stream)
+            last_log_content = get_log_content(event['log_group'], last_log_stream)
         except CLOUDWATCH.exceptions.ResourceNotFoundException as e:
             if 'specified log stream does not exist' not in str(e):
                 raise
 
-    if log_content is None:
-        assert 'Error' in result
-        log_content = get_log_content_from_failed_attempts(json.loads(result['Cause']))
-
-    write_log_to_s3(environ['BUCKET'], event['prefix'], log_content)
+    if last_log_content:
+        write_log_to_s3(bucket, prefix, f'{prefix}.log', last_log_content)
