fe01a2da2a4aae778e212c7bcc016b8b2c9d634e
Jake Herrmann <jtherrmann@alaska.edu>
2024-10-31 15:15:55 -0800

    stack name

--- app.py
+++ app.py
@@ -6,7 +6,5 @@ from cdk_app.hyp3_stack import HyP3Stack
 
 if __name__ == '__main__':
     app = cdk.App()
-    # TODO
-    #HyP3Stack(app, os.environ['STACK_NAME'])
-    HyP3Stack(app, os.environ['hyp3-cdk'])
+    HyP3Stack(app, os.environ['STACK_NAME'])
     app.synth()
