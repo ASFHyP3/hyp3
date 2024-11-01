155ff1e0a6d592a21f278e80a1c26ba143acda7c
Jake Herrmann <jtherrmann@alaska.edu>
2024-11-01 13:41:46 -0800

    get output from nested stack

--- cdk_app/hyp3_stack.py
+++ cdk_app/hyp3_stack.py
@@ -8,8 +8,15 @@ class HyP3Stack(cdk.Stack):
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        cfn_inc.CfnInclude(
+        main_template = cfn_inc.CfnInclude(
             scope=self,
-            id='Template',
+            id='MainStack',
             template_file='packaged.yml',
         )
+
+        # TODO: this isn't needed, but is an example of getting an output from a nested stack
+        cluster_template = main_template.load_nested_stack(
+            'Cluster',
+            template_file='apps/compute-cf.yml'
+        ).included_template
+        print(cluster_template.get_output('JobQueueArn'))
