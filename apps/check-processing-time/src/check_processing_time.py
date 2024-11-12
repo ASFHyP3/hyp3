7c0ef7f2d4ff66de4d769f5ab97dcc2e527a797f
Jake Herrmann <jtherrmann@alaska.edu>
2024-11-12 12:07:13 -0900

    do not bother checking if stop time > start time

--- apps/check-processing-time/src/check_processing_time.py
+++ apps/check-processing-time/src/check_processing_time.py
@@ -5,12 +5,7 @@ def get_time_from_result(result: Union[list, dict]) -> Union[list, float]:
     if isinstance(result, list):
         return [get_time_from_result(item) for item in result]
 
-    processing_time = (result['StoppedAt'] - result['StartedAt']) / 1000
-
-    if processing_time <= 0.0:
-        raise ValueError(f'{processing_time} <= 0.0')
-
-    return processing_time
+    return (result['StoppedAt'] - result['StartedAt']) / 1000
 
 
 def lambda_handler(event, _) -> list[Union[list, float]]:
