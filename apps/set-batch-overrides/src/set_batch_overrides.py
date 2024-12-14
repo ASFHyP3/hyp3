8b2e41c9f2cf653481a8a736d4b2ee5a71929df8
Jake Herrmann <jtherrmann@alaska.edu>
2024-12-13 16:54:46 -0900

    support memory overrides for multi-burst spot jobs

--- apps/set-batch-overrides/src/set_batch_overrides.py
+++ apps/set-batch-overrides/src/set_batch_overrides.py
@@ -65,7 +65,9 @@ def get_insar_isce_burst_memory(job_parameters: dict) -> str:
 def lambda_handler(event: dict, _) -> dict:
     job_type, job_parameters = event['job_type'], event['job_parameters']
 
-    if job_type == 'INSAR_ISCE_MULTI_BURST':
+    # TODO revert
+    #if job_type == 'INSAR_ISCE_MULTI_BURST':
+    if job_type in ('INSAR_ISCE_MULTI_BURST', 'INSAR_ISCE_MULTI_BURST_SPOT'):
         memory = get_insar_isce_burst_memory(job_parameters)
         omp_num_threads = INSAR_ISCE_BURST_OMP_NUM_THREADS[memory]
         return get_container_overrides(memory, omp_num_threads)
