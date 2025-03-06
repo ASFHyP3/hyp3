fc97a77d5a287fbd06a1457c7f0b609f1babd977
jacquelynsmale <jacquelyn.smale@gmail.com>
2025-03-06 09:43:41 -0900

    add in mock submit jobs

--- tests/test_start_execution.py
+++ tests/test_start_execution.py
@@ -202,3 +202,64 @@ def test_lambda_handler_500_jobs():
             call(mock_jobs[0:250]),
             call(mock_jobs[250:500]),
         ]
+
+
+def test_lambda_handler_400_jobs():
+    with (
+        patch('dynamo.jobs.get_jobs_waiting_for_execution') as mock_get_jobs_waiting_for_execution,
+        patch('start_execution.submit_jobs') as mock_submit_jobs,
+        patch.dict(os.environ, {'START_EXECUTION_WORKER_ARN': 'test-worker-function-arn'}, clear=True),
+        patch.dict(os.environ, {'STEP_FUNCTION_ARN': 'test-step-function-arn'}, clear=True)
+    ):
+        mock_jobs = list(range(400))
+        mock_get_jobs_waiting_for_execution.return_value = mock_jobs
+
+        mock_submit_jobs.return_value = None
+
+        start_execution.lambda_handler(None, None)
+
+        mock_get_jobs_waiting_for_execution.assert_called_once_with(limit=500)
+
+        assert mock_submit_jobs.mock_calls == [
+            call(mock_jobs[0:250]),
+            call(mock_jobs[250:400]),
+        ]
+
+
+def test_lambda_handler_50_jobs():
+    with (
+        patch('dynamo.jobs.get_jobs_waiting_for_execution') as mock_get_jobs_waiting_for_execution,
+        patch('start_execution.submit_jobs') as mock_submit_jobs,
+        patch.dict(os.environ, {'START_EXECUTION_WORKER_ARN': 'test-worker-function-arn'}, clear=True),
+        patch.dict(os.environ, {'STEP_FUNCTION_ARN': 'test-step-function-arn'}, clear=True)
+    ):
+        mock_jobs = list(range(50))
+        mock_get_jobs_waiting_for_execution.return_value = mock_jobs
+
+        mock_submit_jobs.return_value = None
+
+        start_execution.lambda_handler(None, None)
+
+        mock_get_jobs_waiting_for_execution.assert_called_once_with(limit=500)
+
+        assert mock_submit_jobs.mock_calls == [
+            call(mock_jobs[0:50])
+        ]
+
+
+def test_lambda_handler_no_jobs():
+    with (
+        patch('dynamo.jobs.get_jobs_waiting_for_execution') as mock_get_jobs_waiting_for_execution,
+        patch('start_execution.submit_jobs') as mock_submit_jobs,
+        patch.dict(os.environ, {'START_EXECUTION_WORKER_ARN': 'test-worker-function-arn'}, clear=True),
+        patch.dict(os.environ, {'STEP_FUNCTION_ARN': 'test-step-function-arn'}, clear=True)
+    ):
+        mock_get_jobs_waiting_for_execution.return_value = []
+
+        mock_submit_jobs.return_value = None
+
+        start_execution.lambda_handler(None, None)
+
+        mock_get_jobs_waiting_for_execution.assert_called_once_with(limit=500)
+
+        mock_submit_jobs.assert_not_called()
