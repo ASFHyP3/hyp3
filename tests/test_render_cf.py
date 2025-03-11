1e7d2a5f4bd7d07ffe18abc5006a7be134210c8c
William Horn <wbhorn@alaska.edu>
2025-03-10 18:50:29 -0800

    Add test to check that cost_parameters must be a list

--- tests/test_render_cf.py
+++ tests/test_render_cf.py
@@ -203,6 +203,13 @@ def test_validate_job_spec():
         job_spec_bad_cost_profile = {**job_spec, 'cost_profiles': {'foo': {}}}
         render_cf.validate_job_spec(job_type, job_spec_bad_cost_profile)
 
+    bad_cost_profile_error = r'Cost definition for job type FOO has invalid cost_parameters: Must be a list of strings'
+    with pytest.raises(ValueError, match=bad_cost_profile_error):
+        job_spec_bad_cost_profile = {**job_spec, 'cost_profiles': {
+            'foo': {'cost_parameters': 'bar', 'cost_table': {'x': 'y'}}
+        }}
+        render_cf.validate_job_spec(job_type, job_spec_bad_cost_profile)
+
     bad_cost_profile_error = r'Cost definition for job type FOO has empty cost_parameters'
     with pytest.raises(ValueError, match=bad_cost_profile_error):
         job_spec_bad_cost_profile = {**job_spec, 'cost_profiles': {
