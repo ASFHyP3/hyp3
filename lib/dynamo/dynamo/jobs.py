e69b5118ab925e07eb81edb116bfcf42529aba33
Jake Herrmann <jtherrmann@alaska.edu>
2025-03-31 14:26:00 -0800

    add todo

--- lib/dynamo/dynamo/jobs.py
+++ lib/dynamo/dynamo/jobs.py
@@ -250,6 +250,7 @@ def update_job_for_user(job_id: str, name: str | None, user_id: str) -> dict:
         job = table.update_item(
             Key={'job_id': job_id},
             UpdateExpression=update_expression,
+            # TODO: explicitly check that job_id exists
             ConditionExpression='user_id = :user_id',
             ExpressionAttributeValues={':user_id': user_id, **name_value},
             ExpressionAttributeNames={'#name': 'name'},
