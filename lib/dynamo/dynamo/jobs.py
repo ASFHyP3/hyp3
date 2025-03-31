e46f4274dcdf9099dc2437cd053652572f78001c
Jake Herrmann <jtherrmann@alaska.edu>
2025-03-31 12:25:15 -0800

    add todo

--- lib/dynamo/dynamo/jobs.py
+++ lib/dynamo/dynamo/jobs.py
@@ -262,6 +262,7 @@ def update_job_for_user(job_id: str, name: str | None, user_id: str) -> dict:
                 raise UpdateJobNotFoundError(f'Job {job_id} does not exist')
             assert e.response['Item']['user_id']['S'] != user_id
             raise UpdateJobForDifferentUserError("You cannot modify a different user's job")
+        # TODO: test the `raise` case?
         raise
 
     return job
