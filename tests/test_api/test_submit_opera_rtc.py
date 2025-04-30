c41a4bd8f2b76a5c1c7503143c30d4a576680c8b
Jake Herrmann <jtherrmann@alaska.edu>
2025-04-30 11:18:33 -0800

    add integration test for OPERA_RTC successful validation

---
+++ tests/test_api/test_submit_opera_rtc.py
@@ -0,0 +1,23 @@
+from http import HTTPStatus
+
+from test_api.conftest import JOBS_URI, login
+
+
+def test_submit_opera_rtc_validate_only(client, tables, approved_user):
+    login(client, username=approved_user)
+
+    response = client.post(
+        JOBS_URI,
+        json={
+            'validate_only': True,
+            'jobs': [
+                {
+                    'job_type': 'OPERA_RTC',
+                    'job_parameters': {'granules': ['S1_118338_IW2_20170102T124017_VV_0675-BURST']},
+                }
+            ],
+        },
+    )
+    assert response.status_code == HTTPStatus.OK
+    jobs = tables.jobs_table.scan()['Items']
+    assert len(jobs) == 0
