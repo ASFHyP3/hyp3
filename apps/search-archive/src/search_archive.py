52ea51d03b637bbe16588cc7b5d7ac4ff7b847ac
Jake Herrmann <jtherrmann@alaska.edu>
2025-07-31 16:07:06 -0800

    add logging to search-archive lambda

--- apps/search-archive/src/search_archive.py
+++ apps/search-archive/src/search_archive.py
@@ -5,16 +5,19 @@ import asf_search as asf
 from asf_enumeration import aria_s1_gunw
 
 import dynamo
+from lambda_logging import log_exceptions, logger
 
 
 def _get_product_from_archive(job_type: str, job_parameters: dict) -> asf.ASFProduct | None:
     if job_type == 'ARIA_S1_GUNW':
+        logger.info(f'Searching archive for {job_type} product')
         return aria_s1_gunw.get_product(
             reference_date=job_parameters['reference_date'],
             secondary_date=job_parameters['secondary_date'],
             frame_id=job_parameters['frame_id'],
         )
     else:
+        logger.info(f'Searching archive is not supported for {job_type}')
         return None
 
 
@@ -39,9 +42,17 @@ def _update_job(job_id: str, product: asf.ASFProduct) -> None:
     )
 
 
+@log_exceptions
 def lambda_handler(event: dict, _) -> bool:
+    logger.info(event)
+
     if product := _get_product_from_archive(event['job_type'], event['job_parameters']):
+        logger.info(f'Found product, updating job {event["job_id"]}')
         _update_job(event['job_id'], product)
+
+        logger.info(f'Refunding {event["credit_cost"]} credits to user {event["user_id"]}')
         dynamo.user.add_credits(event['user_id'], Decimal(event['credit_cost']))
+
         return True
+
     return False
