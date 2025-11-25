02d3cbde9403ad5d99c3138cb4dc5aa6b3a3bdbd
Jake Herrmann <jtherrmann@alaska.edu>
2025-11-24 18:31:36 -0900

    add test_patch_jobs

---
+++ tests/test_api/test_patch_jobs.py
@@ -0,0 +1,131 @@
+from http import HTTPStatus
+
+from test_api.conftest import JOBS_URI, login
+
+
+def test_patch_jobs(client, tables):
+    table_items = [
+        {
+            'job_id': '33d85ea0-9342-4c21-ae59-5bec3f71612c',
+            'name': 'oldname1',
+            'somefield': 'somevalue',
+            'user_id': 'user1',
+        },
+        {
+            'job_id': '40183948-48a1-42d2-a96b-ce44fbba301b',
+            'name': 'oldname2',
+            'somefield': 'somevalue',
+            'user_id': 'user1',
+        },
+        {
+            'job_id': 'b0dd41bc-5597-4a58-afe4-81f30e53bbb0',
+            'name': 'oldname3',
+            'somefield': 'somevalue',
+            'user_id': 'user2',
+        },
+    ]
+
+    for item in table_items:
+        tables.jobs_table.put_item(Item=item)
+
+    login(client, 'user1')
+
+    response = client.patch(
+        JOBS_URI,
+        json={
+            'job_ids': [
+                '33d85ea0-9342-4c21-ae59-5bec3f71612c',
+                '40183948-48a1-42d2-a96b-ce44fbba301b',
+            ],
+            'name': 'newname',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json is None
+    assert tables.jobs_table.scan()['Items'] == [
+        {
+            'job_id': '33d85ea0-9342-4c21-ae59-5bec3f71612c',
+            'name': 'newname',
+            'somefield': 'somevalue',
+            'user_id': 'user1',
+        },
+        {
+            'job_id': '40183948-48a1-42d2-a96b-ce44fbba301b',
+            'name': 'newname',
+            'somefield': 'somevalue',
+            'user_id': 'user1',
+        },
+        {
+            'job_id': 'b0dd41bc-5597-4a58-afe4-81f30e53bbb0',
+            'name': 'oldname3',
+            'somefield': 'somevalue',
+            'user_id': 'user2',
+        },
+    ]
+
+    response = client.patch(
+        JOBS_URI,
+        json={
+            'job_ids': [
+                '33d85ea0-9342-4c21-ae59-5bec3f71612c',
+                '40183948-48a1-42d2-a96b-ce44fbba301b',
+            ],
+            'name': None,
+        },
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json is None
+    assert tables.jobs_table.scan()['Items'] == [
+        {
+            'job_id': '33d85ea0-9342-4c21-ae59-5bec3f71612c',
+            'somefield': 'somevalue',
+            'user_id': 'user1',
+        },
+        {
+            'job_id': '40183948-48a1-42d2-a96b-ce44fbba301b',
+            'somefield': 'somevalue',
+            'user_id': 'user1',
+        },
+        {
+            'job_id': 'b0dd41bc-5597-4a58-afe4-81f30e53bbb0',
+            'name': 'oldname3',
+            'somefield': 'somevalue',
+            'user_id': 'user2',
+        },
+    ]
+
+    response = client.patch(
+        JOBS_URI,
+        json={
+            'job_ids': [
+                '33d85ea0-9342-4c21-ae59-5bec3f71612c',
+                '40183948-48a1-42d2-a96b-ce44fbba301b',
+            ],
+            'name': 'anothernewname',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json is None
+    assert tables.jobs_table.scan()['Items'] == [
+        {
+            'job_id': '33d85ea0-9342-4c21-ae59-5bec3f71612c',
+            'name': 'anothernewname',
+            'somefield': 'somevalue',
+            'user_id': 'user1',
+        },
+        {
+            'job_id': '40183948-48a1-42d2-a96b-ce44fbba301b',
+            'name': 'anothernewname',
+            'somefield': 'somevalue',
+            'user_id': 'user1',
+        },
+        {
+            'job_id': 'b0dd41bc-5597-4a58-afe4-81f30e53bbb0',
+            'name': 'oldname3',
+            'somefield': 'somevalue',
+            'user_id': 'user2',
+        },
+    ]
