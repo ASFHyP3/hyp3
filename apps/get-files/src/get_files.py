53f77c346f19406fa63761d294673922e179b4c0
Andrew Player <andrewplayer3@gmail.com>
2026-03-26 15:39:36 -0400

    cleaner get_files handler

--- apps/get-files/src/get_files.py
+++ apps/get-files/src/get_files.py
@@ -90,15 +90,12 @@ def organize_files(s3_objects: list[dict], bucket: str) -> dict:
     }
 
 
-# FIXME: 
 def lambda_handler(event: dict, context: object) -> None:
-    bucket = environ['BUCKET']
-    job_id = event['job_id']
-    job = dynamo.jobs.get_job(job_id=job_id)
+    job = dynamo.jobs.get_job(job_id=event['job_id'])
 
-    if job['bucket'] is not bucket and bucket is not "":
-        bucket = job['bucket']
-        prefix = job['bucket_prefix']
+    # `bucket` and `bucket_prefix` are set in the `api.handlers.post_jobs`
+    bucket = job['bucket']
+    prefix = job['bucket_prefix']
 
     response = S3_CLIENT.list_objects_v2(Bucket=bucket, Prefix=prefix)
     files = organize_files(response['Contents'], bucket)
