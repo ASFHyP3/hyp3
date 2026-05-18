73624e8c085016e32060863fc9d8a6979233fabe
Joseph H Kennedy <me@jhkennedy.org>
2026-05-18 14:09:04 -0800

    Add OpenAPI spec for bucket-policy

--- apps/api/src/hyp3_api/routes.py
+++ apps/api/src/hyp3_api/routes.py
@@ -196,5 +196,6 @@ def user_get() -> Response:
 
 
 @app.route('/bucket-policy/<bucket_name>', methods=['GET'])
+@openapi
 def bucket_policy_get(bucket_name: str) -> Response:
     return jsonify(handlers.get_bucket_policy(bucket_name))
