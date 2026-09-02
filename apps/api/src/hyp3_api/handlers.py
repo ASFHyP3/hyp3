2d76dbc21b24b347a7ae51f5731d1f68970fcf3e
tcchase <tylercchase@gmail.com>
2026-09-02 16:59:16 -0400

    chore: update time.time() to monotonic, move logic inside while loop

--- apps/api/src/hyp3_api/handlers.py
+++ apps/api/src/hyp3_api/handlers.py
@@ -129,13 +129,12 @@ def _user_response(user_record: dict) -> dict:
 
 def _get_names_for_user(user: str) -> list[str]:
     jobs, next_key = dynamo.jobs.query_jobs(user)
-    start = time.time()
-    current_time = time.time()
-    if next_key:
-        while next_key is not None and current_time - start < 10:
-            new_jobs, next_key = dynamo.jobs.query_jobs(user, start_key=next_key)
-            jobs.extend(new_jobs)
-            current_time = time.time()
+    start = time.monotonic()
+    while next_key is not None:
+        new_jobs, next_key = dynamo.jobs.query_jobs(user, start_key=next_key)
+        jobs.extend(new_jobs)
+        if time.monotonic() - start > 10:
+            break
     names = {job['name'] for job in jobs if 'name' in job}
     return sorted(list(names))
 
